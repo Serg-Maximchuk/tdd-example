b8dee30a54bd932fbac4fd828842913f4c9766ef
Serg-Maximchuk <serg.maximchuk@gmail.com>
2018-08-16 20:17:28 +0300

    Initial commit

---
+++ src/test/java/com/example/tdd/TddApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.tdd;
+
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+public class TddApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
